07e603152a783a2dfb1fc286af0784fd7a9142c7
MichalPokrzywa <michpok467@student.polsl.pl>
2025-01-06 11:21:13 +0100

    Added Troll with Animations

--- Assets/Systems/Troll/TrollAnimation.cs
+++ Assets/Systems/Troll/TrollAnimation.cs
@@ -1,21 +1,47 @@
+using Spine.Unity;
 using System.Collections;
 using System.Collections.Generic;
+using Spine;
 using UnityEngine;
 
 namespace Troll
 {
     public class TrollAnimation : MonoBehaviour
     {
-        // Start is called before the first frame update
-        void Start()
+        private SkeletonAnimation skeletonAnimation;
+        [SerializeField] private bool loopSequence = true; // Whether the sequence should loop
+        private bool isLastAnimation;
+        public List<AnimationReferenceAsset> animationSequence = new List<AnimationReferenceAsset>();
+        private void Start()
         {
-            
+            skeletonAnimation = GetComponent<SkeletonAnimation>();
+
+            // Start looping the animations
+            PlayAnimationSequence();
         }
 
-        // Update is called once per frame
-        void Update()
+        private void PlayAnimationSequence()
         {
-            
+            TrackEntry currentTrack = null;
+
+            // Iterate through the animation sequence
+            for (int i = 0; i < animationSequence.Count; i++)
+            {
+                isLastAnimation = i == animationSequence.Count - 1;
+
+                if (isLastAnimation && loopSequence)
+                {
+                    currentTrack = skeletonAnimation.state.AddAnimation(0, animationSequence[i], false,0);
+                    currentTrack.Complete += _ =>
+                    {
+                        PlayAnimationSequence(); 
+                    };
+                }
+                else
+                {
+                    skeletonAnimation.state.AddAnimation(0, animationSequence[i], false, 0);
+                }
+            }
         }
     }
 }
